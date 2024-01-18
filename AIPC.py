5b9f98b7ad83d0f982bc252efe7007de2c146074
Muhammad Taqi Mirza <tmirza5@outlook.com>
2024-01-18 22:00:01 +0000

    Update AIPC.py
    Added base code

--- AIPC.py
+++ AIPC.py
@@ -11,4 +11,13 @@
 #   distributed under the License is distributed on an "AS IS" BASIS,
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
+import pythonping
+from pythonping import ping
 
+ip_start = int(input("What is the starting IP address? (input the portion after the last d.p)\n"))
+ip_end = int(input("What is the ending IP address? (input the portion after the last d.p)\n"))
+subnet = input("What is the subnet? (Enter the numbers before the last d.p (include the d.ps))\n")
+
+for num in range(ip_start, ip_end):
+    ping(f"{subnet}.{num}", verbose=True)
+    
